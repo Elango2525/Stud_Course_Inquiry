46de26bc8fc3fbb3994f9c51375dcea5acefaae6
ELANGOVAN S <114054446+Elango2525@users.noreply.github.com>
2024-02-09 14:33:32 +0530

    Add files via upload

---
+++ Backend/Day 9/mapper/Bookingmapper.java
@@ -0,0 +1,38 @@
+// package com.home.home.mapper;
+
+
+
+// import com.home.home.dto.AdminDTO;
+// import com.home.home.entity.Admin;
+
+// public class Bookingmapper {
+
+//     public static AdminDTO maptoServicedto(Admin bookingentity) {
+//         // Implement the mapping logic
+//         return new AdminDTO(
+//                 bookingentity.getId(),
+//                 bookingentity.getUsername(),
+//                 bookingentity.getEmail(),
+//                 bookingentity.getPhonenumber(),
+//                 bookingentity.getDate(),
+//                 bookingentity.getServicetype()
+              
+//         );
+//     }
+
+//     public static Admin maptoServiceentity(AdminDTO bookingdto) {
+//         // Implement the mapping logic
+//         return new Admin(
+//                 bookingdto.getId(),
+//                 bookingdto.getUsername(),
+//                 bookingdto.getEmail(),
+//                 bookingdto.getPhonenumber(),
+//                 bookingdto.getDate(),
+//                 bookingdto.getServicetype ()
+                
+//         );
+//     }
+// }
+
+
+
